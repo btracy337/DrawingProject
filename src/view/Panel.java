36284b825a7bc64bd19266a03bbbe69e3144b8ea
Bryan Tracy <btracy337@gmail.com>
2019-05-03 09:35:46 -0600

    Added panel methods.

--- src/view/Panel.java
+++ src/view/Panel.java
@@ -1,9 +1,37 @@
 package view;
 import javax.swing.*;
+import java.awt.*;
+import controller.Controller;
 public class Panel extends JPanel
 {
-public Panel()
+private Controller app;
+public Panel(Controller app)
 {
 	super();
+	this.app = app;
+	setupButtons();
+	setupLayout();
+	setupListeners();
+	setupPanel();
+}
+private void setupButtons()
+{
+	
+}
+private void setupPanel()
+{
+	this.setBackground(Color.blue);
+	
+}
+private void setupLayout()
+{
+	
+	
+}
+private void setupListeners()
+{
+	
+	
+		
 }
 }
