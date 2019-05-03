641bdcfc4fd0cba238bc14906c6963b4712f34fb
Bryan Tracy <btracy337@gmail.com>
2019-05-03 09:26:06 -0600

    Added framework for model, view, controller.

---
+++ src/view/Frame.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.*;
+import controller.Controller;
+
+public class Frame extends JFrame
+{
+	private Controller app;
+
+	public Frame()
+	{
+		super();
+
+	}
+
+	private void setupFrame()
+	{
+
+	}
+}
